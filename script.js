2211da431ad490b43def1e1bf86080ca069e6aa2
elarth31 <artuhovaela4@gmail.com>
2025-02-18 22:17:06 +0200

    burger-menu

--- script.js
+++ script.js
@@ -19,3 +19,21 @@ const scrollToTopBtn = document.getElementById('scrollToTopBtn');
       behavior: 'smooth',
     });
   });
+
+
+
+  const burgerButton = document.querySelector('.burger-menu');
+  const navMenu = document.querySelector('.header__nav');
+  const overlay = document.querySelector('.overlay');
+
+  burgerButton.addEventListener('click', function () {
+    navMenu.classList.toggle('active');
+    overlay.classList.toggle('active');
+    document.body.classList.toggle('no-scroll'); // чтобы не скролить фон
+  });
+
+  overlay.addEventListener('click', function () {
+    navMenu.classList.remove('active');
+    overlay.classList.remove('active');
+    document.body.classList.remove('no-scroll');
+  });
