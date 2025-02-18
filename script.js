2fe167908313552f8993e0c6c88a1e24c696eb62
elarth31 <artuhovaela4@gmail.com>
2025-02-18 21:21:38 +0200

    scroll

---
+++ script.js
@@ -0,0 +1,18 @@
+const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+  window.addEventListener('scroll', function () {
+    if (window.scrollY > 300) {
+      scrollToTopBtn.style.display = 'block';
+      scrollToTopBtn.classList.add('active');
+    } else {
+      scrollToTopBtn.style.display = 'none';
+      scrollToTopBtn.classList.remove('active');
+    }
+  });
+
+  scrollToTopBtn.addEventListener('click', function () {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  });
